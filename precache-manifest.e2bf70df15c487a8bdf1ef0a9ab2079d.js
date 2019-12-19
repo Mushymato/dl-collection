f4ad214eaad58a01b8f4c940597d3dfc06091acb
mushymato <wildshinobu@gmail.com>
2019-12-19 11:51:12 -0500

    Updates

---
+++ precache-manifest.e2bf70df15c487a8bdf1ef0a9ab2079d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32a19bf32940b5d8a0c12efdf0d5ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90841bb1d24467142d72",
+    "url": "/static/css/main.645d2dde.chunk.css"
+  },
+  {
+    "revision": "7a109614ee48ecea1ba4",
+    "url": "/static/js/2.00e5b14d.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.00e5b14d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90841bb1d24467142d72",
+    "url": "/static/js/main.80824f0a.chunk.js"
+  },
+  {
+    "revision": "a1de52adf55a19d33cfe",
+    "url": "/static/js/runtime-main.86031e30.js"
+  }
+]);

795e2d5dcf7a90214fce9a3fdda0d59b1f630508
mushymato <wildshinobu@gmail.com>
2020-06-29 06:10:56 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dl-collection/precache-manifest.f38ba6f8ffab7b5b93888d626755736c.js"
+  "/dl-collection/precache-manifest.c36bd5fd8f826c208b48175e0c93df26.js"
 );
 
 self.addEventListener('message', (event) => {
